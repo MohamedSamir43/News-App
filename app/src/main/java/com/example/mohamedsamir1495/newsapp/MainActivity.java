fed49fe93efcbcfbce842431508ae8512d77d432
Mohamed Samir <mohamedsamir1495@gmail.com>
2018-12-10 02:10:56 +0200

    News App Phase 2
    Final Commit

--- app/src/main/java/com/example/mohamedsamir1495/newsapp/MainActivity.java
+++ app/src/main/java/com/example/mohamedsamir1495/newsapp/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.mohamedsamir1495.newsapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.content.Loader;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,7 +30,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Object> {
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<NewsModel>> {
 
     @BindView(R.id.news_recycler_view)
     RecyclerView newsRecyclerView;
@@ -41,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ButterKnife.bind(this);
         mRecyclerAdapter = new NewsAdapter(new ArrayList<NewsModel>(), this);
         newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -48,8 +57,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         notifyVisablityController();
 
         if (isConnected()) {
-            getSupportLoaderManager().initLoader(0, null, this).forceLoad();
-            Log.e("TAG", "Initiated");
+            {
+                LoaderManager loaderManager = getLoaderManager();
+                loaderManager.initLoader(0, null, this);
+            }
         } else
             Snackbar.make(mParentView, getString(R.string.connectionError), Snackbar.LENGTH_SHORT).show();
 
@@ -62,23 +73,66 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         return (netWorkInfo != null && netWorkInfo.isConnected());
     }
 
-
     @NonNull
     @Override
-    public Loader<Object> onCreateLoader(int i, @Nullable Bundle bundle) {
-        return new ServiceLoader(MainActivity.this);
+    public Loader<List<NewsModel>> onCreateLoader(int i, @Nullable Bundle bundle) {
+        String REQUEST_URL = "http://content.guardianapis.com/search?";
+        String API_KEY = "6f229d90-3108-4e8b-ac10-0744600f329e";
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String minNews = sharedPreferences.getString(getString(R.string.settings_min_news_key), getString(R.string.settings_min_news_default));
+        String orderBy = sharedPreferences.getString(getString(R.string.settings_order_by_key), getString(R.string.settings_order_by_default));
+        String section = sharedPreferences.getString(getString(R.string.settings_section_news_key), getString(R.string.settings_section_news_default));
+
+        Uri baseUri = Uri.parse(REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("api-key", API_KEY);
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("page-size", minNews);
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+
+        if (!section.equals(getString(R.string.settings_section_news_default))) {
+            uriBuilder.appendQueryParameter("section", section);
+        }
+
+        return new ServiceLoader(this, uriBuilder.toString());
     }
 
     @Override
-    public void onLoadFinished(@NonNull Loader<Object> loader, Object o) {
-        Log.e("TAG", "Load Finished");
-        mRecyclerAdapter.updateAdapter((List<NewsModel>) o);
+    public void onLoadFinished(Loader<List<NewsModel>> loader, List<NewsModel> data) {
+        if (data != null && !data.isEmpty()) {
+            mRecyclerAdapter.updateAdapter(data);
+        }
         notifyVisablityController();
     }
 
     @Override
-    public void onLoaderReset(@NonNull Loader<Object> loader) {
+    public void onLoaderReset(Loader<List<NewsModel>> loader) {
+            mRecyclerAdapter.dataModel.clear();
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setting) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     void notifyVisablityController() {
         if (mRecyclerAdapter.getItemCount() > 0) {
--- app/src/main/java/com/example/mohamedsamir1495/newsapp/ServiceLoader.java
+++ app/src/main/java/com/example/mohamedsamir1495/newsapp/ServiceLoader.java
@@ -1,63 +1,32 @@
 package com.example.mohamedsamir1495.newsapp;
 
+import android.content.AsyncTaskLoader;
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.content.AsyncTaskLoader;
-import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
 
 
-public class ServiceLoader extends AsyncTaskLoader<Object> implements Response.Listener<JSONObject>, Response.ErrorListener {
+public class ServiceLoader extends AsyncTaskLoader<List<NewsModel> >{
 
-    final String url = "https://content.guardianapis.com/search?&show-tags=contributor&api-key=6f229d90-3108-4e8b-ac10-0744600f329e&page-size=50";
-    List<NewsModel> resultObject = null;
-    Context mParentContext;
+    String mURL ;
 
-    public ServiceLoader(@NonNull Context context) {
+    public ServiceLoader(Context context, String url) {
         super(context);
-        this.mParentContext = context;
+        this.mURL = url;
     }
 
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
 
-    @Nullable
     @Override
     public List<NewsModel> loadInBackground() {
-        startVolleyService();
-        return (resultObject == null) ? new ArrayList<NewsModel>() : resultObject;
-    }
+        if (mURL == null) {
+            return null;
+        }
 
-
-    void startVolleyService() {
-        RequestQueue mRequestQueue = Volley.newRequestQueue(mParentContext);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, this, this);
-        mRequestQueue.add(jsonObjectRequest);
-    }
-
-
-    @Override
-    public void onResponse(JSONObject response) {
-        resultObject = Deserializer.getList(response, mParentContext);
-
-        Log.e("TAG", resultObject.size() + " ");
-        deliverResult(resultObject);
-
-    }
-
-    @Override
-    public void onErrorResponse(VolleyError error) {
-        deliverCancellation();
+        List<NewsModel> newsList = WebServiceManager.fetchNewsData(mURL);
+        return newsList;
     }
 }
